a95961697c2726ffbd2a8b732b7a6a84a8397a57
Pol-S <plsbnjy@gmail.com>
2020-04-26 15:45:46 -0500

    Displayed data in a list.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
+
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <h1>All quotes </h1>
+    <div v-for="quote in quotes">
+      <br>
+      <h2>"{{ quote.quote }}"</h2>
+      <p>Context: {{ quote.context }}
+      Source: {{ quote.source }}
+      Theme: {{ quote.theme }}</p>
+    </div>
+    {{quotes}}
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
+import axios from "axios";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      quotes: [],
+    };
+  },
+  created: function() {
+    axios
+      .get("https://gist.githubusercontent.com/benchprep/dffc3bffa9704626aa8832a3b4de5b27/raw/quotes.json")
+      .then(response => {
+        this.quotes = response.data;
+      });
+  },
+  methods: {},
+};
 </script>
